47ff9c57a4d515e57da86b656804486f8945ec09
Bjorn <bjorn.jorissen@student.uhasselt.be>
2019-05-06 13:22:37 +0200

    Dictonary, not working though

--- TTISD-Project/Research/RFID/Files/Read_UID/Read_UID.ino
+++ TTISD-Project/Research/RFID/Files/Read_UID/Read_UID.ino
@@ -1,15 +1,23 @@
-#include <ArduinoSTL.h>
-
 /*
   Reading multiple RFID tags, simultaneously!
   By: Nathan Seidle @ SparkFun Electronics
   Date: October 3rd, 2016
   https://github.com/sparkfun/Simultaneous_RFID_Tag_Reader
 
-  Constantly reads and outputs any tags heard
+  An example showing how to read the unique ID from a tag.
+  
+  Every tag has a Tag ID (the TID) that is not editable. This memory
+  contains the chip vendor ID and model ID for the tag (24 bits). All tags
+  must have these bits. Almost all tags also include a UID or unique ID
+  in the TID memory area. 
 
-  If using the Simultaneous RFID Tag Reader (SRTR) shield, make sure the serial slide
-  switch is in the 'SW-UART' position
+  UIDs can range in length. We've seen 8 bytes (64 bit) most commonly but some
+  UIDs are 20 bytes, 160 bits or 1,460,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000 
+  different possible unique IDs
+
+  UIDs can range from 0 to 20 bytes. If the sketch tries to read a UID
+  that is longer than a tag actually has then the read will fail. Start with 8 and increase it
+  if you have a tag with a longer UID.
 */
 
 #include <SoftwareSerial.h> //Used for transmitting to the device
@@ -17,85 +25,57 @@
 SoftwareSerial softSerial(2, 3); //RX, TX
 
 #include "SparkFun_UHF_RFID_Reader.h" //Library for controlling the M6E Nano module
-#include <map>
 RFID nano; //Create instance
 
 void setup()
 {
   Serial.begin(115200);
-  while (!Serial); //Wait for the serial port to come online
+
+  while (!Serial);
+  Serial.println();
+  Serial.println("Initializing...");
 
   if (setupNano(38400) == false) //Configure nano to run at 38400bps
   {
-    Serial.println(F("Module failed to respond. Please check wiring."));
+    Serial.println("Module failed to respond. Please check wiring.");
     while (1); //Freeze!
   }
 
-  nano.setRegion(REGION_EUROPE); //Set to North America
+  nano.setRegion(REGION_NORTHAMERICA); //Set to North America
 
-  nano.setReadPower(1000); //5.00 dBm. Higher values may caues USB port to brown out
+  nano.setReadPower(500); //5.00 dBm. Higher values may cause USB port to brown out
   //Max Read TX Power is 27.00 dBm and may cause temperature-limit throttling
 
-  Serial.println(F("Press a key to begin scanning for tags."));
-  while (!Serial.available()); //Wait for user to send a character
-  Serial.read(); //Throw away the user's character
-
-  nano.startReading(); //Begin scanning for tags
+  nano.enableDebugging(); //Turns on commands sent to and heard from RFID module
 }
-
-std::map<String, int> epcs;
-#define WAIT_MS 2000
-int stamp, prev = 0;
 
 void loop()
 {
-  stamp = millis();
+  Serial.println(F("Get one tag near the reader. Press a key to read a tag's unique ID."));
+  while (!Serial.available()); //Wait for user to send a character
+  Serial.read(); //Throw away the user's character
+
+  byte response;
+  byte myUID[8]; //UIDs can range from 0 to 20 bytes. Start with 8.
+  byte uidLength = sizeof(myUID);
   
-  if (nano.check() == true) //Check to see if any new data has come in from module
+  //Read unique ID of tag
+  response = nano.readTID(myUID, uidLength);
+  if (response == RESPONSE_SUCCESS)
   {
-    byte responseType = nano.parseResponse(); //Break response into tag ID, RSSI, frequency, and timestamp
-
-    if (responseType == RESPONSE_IS_KEEPALIVE)
+    Serial.println("UID read!");
+    Serial.print("UID: [");
+    for(byte x = 0 ; x < uidLength ; x++)
     {
-      Serial.println(F("Scanning"));
+      if(myUID[x] < 0x10) Serial.print("0");
+      Serial.print(myUID[x], HEX);
+      Serial.print(" ");
     }
-    else if (responseType == RESPONSE_IS_TAGFOUND)
-    {
-      byte tagEPCBytes = nano.getTagEPCBytes(); //Get the number of bytes of EPC from response
-
-      String stringOne = "";
-      for (byte x = 0 ; x < tagEPCBytes ; x++)
-      {
-        if (nano.msg[31 + x] < 0x10) stringOne = String(stringOne + F("0")); //Pretty print
-        stringOne = String(stringOne + String(nano.msg[31 + x], HEX));
-        stringOne = String(stringOne + F(" "));
-      }
-      Serial.print(stringOne);
-      epcs.[stringOne] = millis();
-      
-      Serial.println();
-    }
-    else if (responseType == ERROR_CORRUPT_RESPONSE)
-    {
-      Serial.println("Bad CRC");
-    }
-    else
-    {
-      //Unknown response
-      Serial.print("Unknown error");
-    }
-     Serial.println(epcs.size());
+    Serial.println("]");
   }
+  else
+    Serial.println("Failed read");
 
-  for(std::map<String, int>::value_type& x : epcs)
-  {
-    if(millis() - x.second < WAIT_MS)
-    {
-      epcs.erase(x.first);
-    }
-  }
-
-  delay(10);
 }
 
 //Gracefully handles a reader that is already configured and already reading continuously
@@ -107,13 +87,13 @@ boolean setupNano(long baudRate)
   //Test to see if we are already connected to a module
   //This would be the case if the Arduino has been reprogrammed and the module has stayed powered
   softSerial.begin(baudRate); //For this test, assume module is already at our desired baud rate
-  while (softSerial.isListening() == false); //Wait for port to open
+  while(!softSerial); //Wait for port to open
 
   //About 200ms from power on the module will send its firmware version at 115200. We need to ignore this.
-  while (softSerial.available()) softSerial.read();
-
+  while(softSerial.available()) softSerial.read();
+  
   nano.getVersion();
-
+  
   if (nano.msg[0] == ERROR_WRONG_OPCODE_RESPONSE)
   {
     //This happens if the baud rate is correct but the module is doing a ccontinuous read
@@ -131,8 +111,6 @@ boolean setupNano(long baudRate)
     nano.setBaud(baudRate); //Tell the module to go to the chosen baud rate. Ignore the response msg
 
     softSerial.begin(baudRate); //Start the software serial port, this time at user's chosen baud rate
-
-    delay(250);
   }
 
   //Test the connection
@@ -144,6 +122,6 @@ boolean setupNano(long baudRate)
 
   nano.setAntennaPort(); //Set TX/RX antenna ports to 1
 
-  
   return (true); //We are ready to rock
 }
+
