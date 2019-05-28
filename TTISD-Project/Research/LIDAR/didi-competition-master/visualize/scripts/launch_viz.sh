4d1a98d3ab4ee8ece659b6e305641e9af700c63f
Wosser1sProductions <wosser1@hotmail.com>
2019-05-28 19:33:49 +0200

    Deploy :camera: :arrow_forward:

--- TTISD-Project/Research/LIDAR/didi-competition-master/visualize/scripts/launch_viz.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-roscore&
-roslaunch --wait velodyne_pointcloud 32e_points.launch&
-rosrun rviz rviz -f velodyne -d /scripts/default.rviz&
-sleep 3
-echo "Play bag file w/ tracklet using 'python scripts/play.py <bag_filename> <tracklet_filename>'"
--- TTISD-Project/Research/LIDAR/didi-competition-master/visualize/viz_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# setup ros environment
-source "/opt/ros/$ROS_DISTRO/setup.bash"
-source "/workspace/install/setup.bash"
-source /scripts/launch_viz.sh
-exec "$@"
