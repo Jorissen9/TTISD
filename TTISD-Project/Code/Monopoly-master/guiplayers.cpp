b647be2ac3855dbbd240550fdabd0eea51e36023
Bjorn <bjorn.jorissen@student.uhasselt.be>
2019-05-28 17:06:31 +0200

    Small changes

--- TTISD-Project/Code/Monopoly-master/guiplayers.cpp
+++ TTISD-Project/Code/Monopoly-master/guiplayers.cpp
@@ -134,6 +134,7 @@ void GUIPlayers::enableUpgrade(){
             ownedProperties[i]->setEnabled(false);
         } else {
             ownedProperties[i]->setEnabled(true);
+            ownedProperties[i]->setFocus();
         }
     }
 }
